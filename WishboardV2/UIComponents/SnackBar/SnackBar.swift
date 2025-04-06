bd012aabc27e9e4e2f5940a8fd9f50c66ee779f9
gom1n <alice436777@gmail.com>
2025-04-06 14:56:14 +0900

    [2qa] 전체 > 스낵바 높이 조절

--- WishboardV2/UIComponents/SnackBar/SnackBar.swift
+++ WishboardV2/UIComponents/SnackBar/SnackBar.swift
@@ -14,7 +14,7 @@ final class SnackBar {
     static var shared = SnackBar()
     
     let SNACKBAR_HEIGHT = 48
-    let SNACKBAR_INTERVAL = 16 + 56
+    let SNACKBAR_INTERVAL = 106
     let TRANSLATION_Y: CGFloat
     
     var window: UIViewController?
