a359e54d25ac23d948e94b95dd2c36fa4ab9286a
gom1n <alice436777@gmail.com>
2025-07-12 21:45:44 +0900

    [chore] 상품 등록 UI 꾸미기 위해 주석 처리했던 것 제거

--- WishboardV2/SceneDelegate.swift
+++ WishboardV2/SceneDelegate.swift
@@ -24,9 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(showSnackBar), name: .ShowSnackBar, object: nil)
         
         // MARK: Navigation controller
-        // TODO: 요거 주석
-//        let splashVC = SplashViewController()
-        let splashVC = AddViewController(type: .manual)
+        let splashVC = SplashViewController()
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
@@ -42,12 +40,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             self.window?.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
         }
         
-        // TODO: 요거 주석
-//        // 일정 시간 후에 온보딩 화면으로 전환
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//            // 앱 버전 체크 후 화면이동
-//            splashVC.checkAppVersion()
-//        }
+        // 일정 시간 후에 온보딩 화면으로 전환
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            // 앱 버전 체크 후 화면이동
+            splashVC.checkAppVersion()
+        }
     }
     
     /// 네트워크 에러 발생 시 온보딩으로 이동
@@ -86,11 +83,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     /// 온보딩 화면 설정
     private func setRootOnboarding() {
-        // TODO: 요거 주석
-//        let onboardingViewController = OnboardingViewController()
-//        let navigationController = UINavigationController(rootViewController: onboardingViewController)
-//        self.window?.rootViewController = navigationController
-//        self.window?.makeKeyAndVisible()
+        let onboardingViewController = OnboardingViewController()
+        let navigationController = UINavigationController(rootViewController: onboardingViewController)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
