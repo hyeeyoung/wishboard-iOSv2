38ea2c0e10890a0de972dcc54eef631bb94834e7
gom1n <alice436777@gmail.com>
2025-09-20 19:50:03 +0900

    [fix] 아이템 수정 시 409 에러 토스트 문구 요청사항 반영

--- Core/Core/Enums/SnackBarType.swift
+++ Core/Core/Enums/SnackBarType.swift
@@ -53,7 +53,7 @@ public enum SnackBarType {
         case .imageLimit: return "앗, 이미지는 최대 10장까지만 등록할 수 있어요!"
         case .invalidUser: return "앗, 이용할 수 없는 계정입니다!\n다시 로그인해 주세요."
         case .logoutByDeviceOverflow: return "최대 3대 기기에서만 로그인할 수 있어\n현재 기기에서 로그아웃되었어요."
-        case .otherUserModifying: return "현재 다른 사용자가 해당 아이템을 수정 중입니다. 잠시 후 다시 시도해주세요."
+        case .otherUserModifying: return "변경사항을 저장하지 못했어요!\n잠시 후 다시 시도해주세요."
             
         case .test: return "로그아웃"
         }
