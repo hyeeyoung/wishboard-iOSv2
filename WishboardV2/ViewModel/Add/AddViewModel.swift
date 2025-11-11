97129d2583e3a1c075b9fb8ebf61167194249f38
gom1n <alice436777@gmail.com>
2025-11-12 00:33:07 +0900

    [fix] 아이템 상세 화면 이후 뎁스에서는 폴더리스트 조회 시 /folder/list 로 호출 및 페이징 로직 제거

--- WishboardV2/ViewModel/Add/AddViewModel.swift
+++ WishboardV2/ViewModel/Add/AddViewModel.swift
@@ -34,13 +34,6 @@ final class AddViewModel {
     @Published var selectedAlarm: String? = nil
     @Published var folders: [FolderListResponse] = []
     
-    // Paging
-    @Published var isLoading: Bool = false
-    @Published var isRefreshing: Bool = false
-    @Published var hasMore: Bool = true
-    private var page: Int = 0          // 서버의 data.number (0-based)
-    private let pageSize: Int = 10     // 서버의 data.size와 일치
-    
     // 저장 버튼 활성화 여부
     var isSaveEnabled: AnyPublisher<Bool, Never> {
         return Publishers.CombineLatest3(itemNamePublisher, itemPricePublisher, selectedImagePublisher)
@@ -140,36 +133,13 @@ final class AddViewModel {
     // 폴더 데이터 가져오기
     /// 폴더 가져오기
     func fetchFolders(reset: Bool = false) async throws {
-        guard !isLoading, hasMore || reset else { return }
-        isLoading = true
-
-        if reset {
-            page = 0
-            hasMore = true
-        }
         do {
-            let usecase = GetFoldersUseCase()
-            let response = try await usecase.execute(page: page, size: pageSize)
-
-            if reset { folders.removeAll() }
-
-            if let itemDatas = response.data?.content {
-                folders.append(contentsOf: itemDatas)
-            }
-
-            // 다음 페이지 여부 및 page 증가
-            hasMore = !(response.data?.last ?? true)
-            if hasMore {
-                page += 1
-            }
-
-            isLoading = false
-            isRefreshing = false
+            let usecase = GetFolderListUseCase()
+            let response = try await usecase.execute()
+            folders = response
         } catch {
-            if reset { folders = [] }
-            isLoading = false
-            isRefreshing = false
-            // 필요하면 에러 상태 @Published 추가해서 바인딩
+            folders = []
+            throw error
         }
     }
 
